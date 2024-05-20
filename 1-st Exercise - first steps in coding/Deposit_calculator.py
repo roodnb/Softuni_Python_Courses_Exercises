8ca11c05668c73a9c2cdf27e94d2fd724f12380b
Jordan Antonov <38000463+roodnb@users.noreply.github.com>
2024-05-20 20:25:10 +0300

    Delete 1-st Exercise - first steps in coding directory

--- 1-st Exercise - first steps in coding/Deposit_calculator.py
+++
@@ -1,7 +0,0 @@
-deposit_sum = float(input())
-deposit_time_frame = int(input())
-annual_interest_rate = float(input()) / 100
-
-sum = deposit_sum + deposit_time_frame * ((deposit_sum * annual_interest_rate) / 12)
-
-print(sum)
